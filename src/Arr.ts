5ed891eea48891cf6f5be1b7e2077c5ecab11a1d
JonathanTheZero <jonathan.frohne@web.de>
2020-10-07 15:19:55 +0200

    unshift, reverse, join and more

--- src/Arr.ts
+++ src/Arr.ts
@@ -1,6 +1,6 @@
 type Nullable<T> = T | null | undefined;
 
-export default class Arr<T> {
+export class Arr<T> {
     [x: number]: Nullable<T>;
     #length: number = 0;
 
@@ -15,11 +15,14 @@ export default class Arr<T> {
 
     push(...el: Nullable<T>[]) {
         let i = Object.keys(this).length;
-        for (let a of el) this[i++] = a;
+        for (let a of el)
+            this[i++] = a;
         this.#length += el.length;
     }
 
     get length() {
+        if (Object.keys(this).length != this.#length)
+            this.#length = Object.keys(this).length;
         return this.#length;
     }
 
@@ -31,12 +34,65 @@ export default class Arr<T> {
         } else if (l > this.#length)
             for (let i = this.#length; i < l; ++i)
                 this[i] = undefined;
+        this.#length = l;
     }
 
     toString(): string {
         let str = "";
-        for (let i in this) str += `, ${this[i]}`;
+        for (let i in this)
+            str += `, ${this[i]}`;
         return str.slice(2, str.length);
+    }
+
+    valueOf(): number {
+        return this.#length;
+    }
+
+    forEach(callback: (value: Nullable<T>, index: number, arr: this) => any): void {
+        let i = 0;
+        for (let p in this)
+            callback(this[p], i++, this);
+    }
+
+    //would be nicer to define without [] but that's not possible
+    concat(...p: Arr<T>[]): number {
+        for (let a of p)
+            if (a)
+                for (let e of a) this.push(e);
+        return this.#length;
+    }
+
+    pop(): Nullable<T> {
+        if (this.#length == 0)
+            return undefined;
+        let a: Nullable<T> = this[this.#length - 1];
+        delete this[this.#length - 1];
+        this.#length -= 1;
+        return a;
+    }
+
+    join(seperator = ",") {
+        let str = "";
+        for (let i in this)
+            str += seperator + this[i];
+        return str;
+    }
+
+    reverse(): Arr<T> {
+        for (let i = 0; i < this.#length; ++i)
+            this.unshift(this.pop());
+        return this;
+    }
+
+    /*shift(): Nullable<T> {
+        return this[0];
+    }*/
+
+    unshift(el: Nullable<T>): number {
+        for (let i = this.#length++; i >= 0; --i)
+            this[i] = this[i - 1];
+        this[0] = el;
+        return this.#length;
     }
 
     [Symbol.iterator]() {
@@ -52,23 +108,7 @@ export default class Arr<T> {
         }
     }
 
-    forEach(callback: (value: Nullable<T>, index: number, arr: this) => any): void {
-        let i = 0;
-        for (let p in this) callback(this[p], i++, this);
+    static isArr(el: any) {
+        return el instanceof Arr;
     }
-
-    concat(a: Arr<T>): number {
-        for (let e of a) this.push(e);
-        return this.#length;
-    }
-
-    pop(): Nullable<T> {
-        if (this.#length == 0) return undefined;
-        let a: Nullable<T> = this[this.#length - 1];
-        delete this[this.#length - 1];
-        this.#length -= 1;
-        return a;
-    }
-
-    static isArr = (el: any) => el instanceof Arr;
 }
--- src/run.ts
+++ src/run.ts
@@ -1,21 +1,9 @@
-import Arr from "./Arr";
+import { Arr } from "./";
 
-/*const a = new Arr<number>(2, 4, 3, 53, 1);
-a.push(1, 2, 3);
+const a = new Arr<number>(1, 2, 3);
+a.length = 10;
 console.log(a);
-for(let b of a){
-    console.log(b);
-}
-
-console.log(...a);
-
-a.concat(new Arr<number>(1, 2, 3));
 console.log(a.length);
+a.unshift(3);
 console.log(a);
-console.log(a.pop());
-console.log(a);*/
-
-const b = new Arr({ a: 7 }, { a: 8});
-console.log(b);
-console.log(b.pop());
-console.log(b);
+console.log(a.length);
